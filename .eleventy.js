1bffe8c4bfad310c9026573945f72f17fac56e16
Johannes Ganzenmueller <johannes.ganzenmueller@autoscout24.com>
2022-04-01 23:24:30 +0200

    Added two-level-paged archive for categories and link to them from posts.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const lodash = require("lodash");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
@@ -30,6 +31,86 @@ module.exports = function(eleventyConfig) {
     }).join(', ');
 
     return `<img loading="lazy" src="${src}" srcset="${srcset}" class="${className ? className : ''}" sizes="${sizes ? sizes : '100vw'}" alt="${alt ? alt : ''}">`;
+  });
+
+    function getAllKeyValues(collectionArray, key) {
+      // get all values from collection
+      let allValues = collectionArray.map((item) => {
+        let values = item.data[key] ? item.data[key] : [];
+        return values;
+      });
+
+      // flatten values array
+      allValues = lodash.flattenDeep(allValues);
+      // to lowercase
+      allValues = allValues.map((item) => item.toLowerCase());
+      // remove duplicates
+      allValues = [...new Set(allValues)];
+      // order alphabetically
+      allValues = allValues.sort(function (a, b) {
+        return a.localeCompare(b, "en", { sensitivity: "base" });
+      });
+      // return
+      return allValues;
+    }
+
+    function strToSlug(str) {
+      return str.toLowerCase();
+    }
+
+  // create flattened paginated blogposts per categories collection
+  // based on Zach Leatherman's solution - https://github.com/11ty/eleventy/issues/332
+  eleventyConfig.addCollection("blogpostsByCategories", function (collection) {
+    const itemsPerPage = 2;
+    let blogpostsByCategories = [];
+    let allBlogposts = collection
+      .getFilteredByGlob("./src/posts/*.md")
+      .reverse();
+    let blogpostsCategories = getAllKeyValues(allBlogposts, "categories");
+
+    // walk over each unique category
+    blogpostsCategories.forEach((category) => {
+      let sanitizedCategory = lodash.deburr(category).toLowerCase();
+      // create array of posts in that category
+      let postsInCategory = allBlogposts.filter((post) => {
+        let postCategories = post.data.categories ? post.data.categories : [];
+        let sanitizedPostCategories = postCategories.map((item) =>
+          lodash.deburr(item).toLowerCase()
+        );
+        return sanitizedPostCategories.includes(sanitizedCategory);
+      });
+
+      // chunck the array of posts
+      let chunkedPostsInCategory = lodash.chunk(postsInCategory, itemsPerPage);
+
+      // create array of page slugs
+      let pagesSlugs = [];
+      for (let i = 0; i < chunkedPostsInCategory.length; i++) {
+        let categorySlug = strToSlug(category);
+        let pageSlug = i > 0 ? `${categorySlug}/${i + 1}` : `${categorySlug}`;
+        pagesSlugs.push(pageSlug);
+      }
+
+      // create array of objects
+      chunkedPostsInCategory.forEach((posts, index) => {
+        blogpostsByCategories.push({
+          title: category,
+          slug: pagesSlugs[index],
+          pageNumber: index,
+          totalPages: pagesSlugs.length,
+          pageSlugs: {
+            all: pagesSlugs,
+            next: pagesSlugs[index + 1] || null,
+            previous: pagesSlugs[index - 1] || null,
+            first: pagesSlugs[0] || null,
+            last: pagesSlugs[pagesSlugs.length - 1] || null,
+          },
+          items: posts,
+        });
+      });
+    });
+
+    return blogpostsByCategories;
   });
 
   return {
--- README.md
+++ README.md
@@ -10,6 +10,7 @@ This is a play project to test and evaluate [Eleventy](https://www.11ty.dev/).
 - Using [markdown-it-attrs](https://github.com/arve0/markdown-it-attrs) to be able to define css-classes in markdown, like in Kramdown at Jekyll
 - ~~Using [eleventy-respimg](https://github.com/eeeps/eleventy-respimg) for serving images via cloudinary.~~
   - seems to be dead and missing important features like local-mode, so created an own one
+- Two-level-paging is based on [this article](https://www.webstoemp.com/blog/basic-custom-taxonomies-with-eleventy/)
 
 ## How to run it locally
 1) Install node
